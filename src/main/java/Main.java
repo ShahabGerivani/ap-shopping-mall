d3d57bcb6eb0ebbef37b8c1b925f5ca4e484458e
Shahab <garivanishahabedin@gmail.com>
2024-06-16 14:50:25 +0330

    Add Main and MainFrame classes

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+// The starting point of the app
+public class Main {
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+}
---
+++ src/main/java/MainFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    MainFrame() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Shop");
+        this.setSize(800, 600);
+        this.setVisible(true);
+        this.setResizable(false);
+    }
+}
