b900dfb98ff11a9502b3fb9b5da54eb169dad99f
Shahab <garivanishahabedin@gmail.com>
2024-06-24 14:58:09 +0330

    Add sign up functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import java.sql.SQLException;
+
 // The starting point of the app
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         new MainFrame();
     }
 }
