cc0d57d1c8708de670fe617c03d8b513711dbef2
Shahab <garivanishahabedin@gmail.com>
2024-06-26 20:58:19 +0330

    Create a new method in ProductsDBManager for submitting a new rating for a product.

--- src/main/java/ProductsDBManager.java
+++ src/main/java/ProductsDBManager.java
@@ -67,4 +67,12 @@ public class ProductsDBManager {
         insertNewProductStatement.setString(5, imageFileName);
         insertNewProductStatement.executeUpdate();
     }
+
+    public void submitNewRating(String username, int product_id, int rating) throws SQLException {
+        PreparedStatement submitRatingStatement = dbConnection.prepareStatement("INSERT INTO ratings (user_username, product_id, rating) VALUES (?, ?, ?)");
+        submitRatingStatement.setString(1, username);
+        submitRatingStatement.setInt(2, product_id);
+        submitRatingStatement.setInt(3, rating);
+        submitRatingStatement.executeUpdate();
+    }
 }
