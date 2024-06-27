ae8a6a17f2a2e88e9d10348609ad4d28f9030f63
Amir Mohammad Zaker <amzzaaker@gmail.com>
2024-06-27 08:28:45 -0700

    Complete UserMainPanel gui and update ProductCardPanel

--- src/main/java/ProductCardPanel.java
+++ src/main/java/ProductCardPanel.java
@@ -1,5 +1,6 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -7,7 +8,7 @@ import java.io.IOException;
 import java.sql.Connection;
 
 public class ProductCardPanel extends JPanel {
-    File file = new File("C:\\Users\\USER\\Desktop\\capsule_616x353.jpg");
+    File file ;
 
     ProductCardPanel(JFrame frame, Connection dbConnection,User user,Product product,JPanel panel) {
         JLabel imageLabel = new JLabel();
@@ -19,9 +20,16 @@ public class ProductCardPanel extends JPanel {
         gbc.gridx = 0;
         gbc.gridy = 0;
         try {
-            BufferedImage bufferedImage = ImageIO.read(this.file);
-            ImageIcon imageIcon = new ImageIcon(bufferedImage.getScaledInstance(200, 200, Image.SCALE_DEFAULT));
-            imageLabel.setIcon(imageIcon);
+            if (file!=null) {
+                BufferedImage bufferedImage = ImageIO.read(this.file);
+                ImageIcon imageIcon = new ImageIcon(bufferedImage.getScaledInstance(200, 200, Image.SCALE_DEFAULT));
+                imageLabel.setIcon(imageIcon);
+            }
+            else {
+                imageLabel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+                imageLabel.setFont((new Font("Arial", Font.PLAIN, 20)));
+                imageLabel.setText("عکس موجود نیست");
+            }
         } catch (IOException e) {
             imageLabel.setFont((new Font("Arial", Font.PLAIN, 20)));
             imageLabel.setText("خطا در نمایش عکس");
@@ -60,7 +68,7 @@ public class ProductCardPanel extends JPanel {
         gbc.gridx=0;
         gbc.gridy=3;
 
-        JLabel productRateField = new JLabel(String.valueOf(product.getRating()));
+        JLabel productRateField = new JLabel(String.format("%.2g%n", product.getRating()));
         productRateField.setFont(new Font("Arial", Font.PLAIN, 20));
         this.add(productRateField,gbc);
 
@@ -73,11 +81,14 @@ public class ProductCardPanel extends JPanel {
         gbc.gridy=4;
         gbc.gridx=0;
         gbc.gridwidth=2;
+        gbc.insets=new Insets(0,0,6,0);
 
         JButton viewButton = new JButton("مشاهده");
         viewButton.setFont(new Font("Arial", Font.PLAIN, 20));
         viewButton.setPreferredSize(new Dimension(100,35));
         viewButton.setFocusable(false);
         this.add(viewButton,gbc);
+        Border blackLine = BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK);
+        this.setBorder(blackLine);
     }
 }
