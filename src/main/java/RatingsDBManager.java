d2ea5e4275b08a771b4c21dcbc4128d3025180b9
Shahab <garivanishahabedin@gmail.com>
2024-06-28 01:12:59 +0330

    Create a method in RatingsDBManager to get a user's rating for a product.

--- src/main/java/RatingsDBManager.java
+++ src/main/java/RatingsDBManager.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RatingsDBManager {
@@ -19,5 +20,17 @@ public class RatingsDBManager {
         submitRatingStatement.setInt(2, product_id);
         submitRatingStatement.setInt(3, rating);
         submitRatingStatement.executeUpdate();
+    }
+
+    public int getRating(String username, int product_id) throws SQLException {
+        PreparedStatement getRatingStmt = dbConnection.prepareStatement("SELECT rating FROM ratings WHERE user_username = ? AND product_id = ?");
+        getRatingStmt.setString(1, username);
+        getRatingStmt.setInt(2, product_id);
+        ResultSet getRatingRs = getRatingStmt.executeQuery();
+        if (getRatingRs.next()) {
+            return getRatingRs.getInt("rating");
+        } else {
+            return 0;
+        }
     }
 }
