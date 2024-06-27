506002d5d1e493f096b169ce61b81423aecca3aa
Shahab <garivanishahabedin@gmail.com>
2024-06-27 12:10:22 +0330

    Create UsersDBManager to manage database interactions for users & Create a method to get a user by username.

---
+++ src/main/java/UsersDBManager.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UsersDBManager {
+    private final Connection dbConnection;
+
+    public UsersDBManager(Connection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
+    public Connection getDbConnection() {
+        return dbConnection;
+    }
+
+    public User getUser(String username) throws SQLException {
+        User user = null;
+
+        PreparedStatement getUserStmt = dbConnection.prepareStatement("SELECT * FROM users WHERE username = ?");
+        getUserStmt.setString(1, username);
+        ResultSet userRs = getUserStmt.executeQuery();
+
+        if (userRs.next()) {
+            user = new User(
+                    userRs.getString("username"),
+                    userRs.getString("name"),
+                    userRs.getString("phone"),
+                    userRs.getString("address"),
+                    userRs.getDouble("balance"),
+                    userRs.getBoolean("is_admin")
+            );
+        }
+
+        return user;
+    }
+}
