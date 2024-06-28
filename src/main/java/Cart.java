ab45245614dacf6f606e369c6824a94b2973c81b
Shahab <garivanishahabedin@gmail.com>
2024-06-28 12:23:33 +0330

    Add id field to Cart

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,13 +1,19 @@
 import java.util.HashMap;
 
 public class Cart {
+    private final int id;
     private final String userUsername;
     private HashMap<Product, Integer> productsAndCount;
     private double total;
 
-    public Cart(String userUsername, HashMap<Product, Integer> productsAndCount) {
+    public Cart(int id, String userUsername, HashMap<Product, Integer> productsAndCount) {
+        this.id = id;
         this.userUsername = userUsername;
         this.productsAndCount = productsAndCount;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getUserUsername() {
